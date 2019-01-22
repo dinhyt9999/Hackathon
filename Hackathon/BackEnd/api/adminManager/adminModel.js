fe9865630f770274f4063b4d794c2baf493d727e
dinhyt9999 <maybaygiay1998@gmail.com>
2019-01-22 21:21:39 +0700

    .

--- Hackathon/BackEnd/api/adminManager/adminModel.js
+++ Hackathon/BackEnd/api/adminManager/adminModel.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const AdminSchema = new Schema({
     user: {type: String, unique: true},
-    pass: {type: String, unique: true}
+    pass: {type: String, unique: true},
+    role: {type: String, unique: true, default: "user"}
 })
 
 module.exports = mongoose.Schema("Admin",AdminSchema)
